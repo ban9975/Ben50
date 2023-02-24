7c1f1928abff44d7b83c58f237a9d3d2f8aff3f1
ban9975 <s9960733@gmail.com>
2023-02-24 17:07:07 +0800

    8 resistance

--- BT_4Resistance/BT_4Resistance.ino
+++ BT_4Resistance/BT_4Resistance.ino
@@ -1,10 +1,10 @@
 #include <SoftwareSerial.h>
 #include <Wire.h>
-#include <Adafruit_ADS1X15.h>
+#include <ADS1X15.h>
 
 SoftwareSerial BTSerial(8, 9);
 //SoftwareSerial BTSerial(10,11);
-Adafruit_ADS1015 ads;
+ADS1015 ads1(0x49);
 bool start = false;
 byte btRead;
 unsigned int anaIn;
@@ -12,7 +12,7 @@ int16_t adc[4];
 void setup() {
   BTSerial.begin(38400);
   Serial.begin(9600);
-  ads.begin();
+  ads1.begin();
 }
 
 void BTPrint(unsigned int);
@@ -31,7 +31,8 @@ void loop() {
       for(byte j = 0; j < btRead; ++j) {
         for(byte i = 0; i < 20; ++i) {
           for(byte k = 0; k < 4; ++k) {
-            adc[k] = ads.readADC_SingleEnded(k);
+            adc[k] = ads1.readADC(k);
+//            filter out abnormal value
 //            while(adc[k] >= 1400) {
 //              adc[k] = ads.readADC_SingleEnded(k);
 //            }
--- BT_pyServer/BTRes_print.py
+++ BT_pyServer/BTRes_print.py
@@ -4,20 +4,21 @@ import interface
 interf = interface.interface()
 
 def main():
+    nSensor = 8
     row = 1
     iter = int(input("Please input the number of iterations: "))
     while iter != 0:
         interf.write(str(iter))
         for i in range(iter):
-            avg = [0, 0, 0, 0]
+            avg = [0, 0, 0, 0, 0, 0, 0, 0]
             for j in range(20):
-                for k in range(4):
+                for k in range(nSensor):
                     btIn = float(interf.read())
                     while btIn == 0:
                         print(0)
                         btIn = float(interf.read())
                     avg[k] += btIn
-            for k in range(4):
+            for k in range(nSensor):
                 avg[k] /= 20
                 res = 100 * avg[k] * 3 / (5000 - avg[k] * 3)
                 print(round(res, 2), end='\t')
