d5819a0beb93e6d3f99af96c81dab1d20c96986f
Hsiao-Chien-Ti <0410adi@gmail.com>
2023-03-17 12:09:51 +0800

    merge

--- rfRaw.py
+++ rfRaw.py
@@ -4,15 +4,6 @@ from sklearn import metrics
 
 import importDataRaw
 modes=['gesture','length', 'raw data']
-<<<<<<< HEAD
-# mode = int(input("0: gesture, 1: length, 2: raw: ")) # 0 for gesture, 1 for length
-mode = 0
-trainFile = 'wristband/v4Plus/adi_v2_1ADC.xlsx'
-train = importDataRaw.importData(trainFile,mode)
-test = importDataRaw.importData('wristband/v4Plus/adi_v2_1ADC_test.xlsx',mode)
-train.data.to_excel(r'data_cal/v4Plus_v2_1ADC_train.xlsx')
-test.data.to_excel(r'data_cal/v4Plus_v2_1ADC_test.xlsx')
-=======
 mode = int(input("0: gesture, 1: length, 2: raw: ")) # 0 for gesture, 1 for length
 # mode = 0
 trainFile = 'wristband/v4Plus/adi_v2_1ADC_sticky_real.xlsx'
@@ -21,7 +12,6 @@ test = importDataRaw.importData('wristband/v4Plus/adi_v2_1ADC_sticky_real_test.x
 if mode == 0:   
     train.data.to_excel(r'data_cal/adi_v2_1ADC_sticky_real_caltrain.xlsx')
     test.data.to_excel(r'data_cal/adi_v2_1ADC_sticky_real_caltest.xlsx')
->>>>>>> 0511199968399dfcfd7e4f7825d084b2acd92c5f
 print(trainFile)
 
 x_train, x_test, y_train, y_test = train_test_split(train.features,train.labels,train_size=0.7, random_state=9999)
